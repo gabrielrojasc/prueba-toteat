c2845fa06673f47def81280a6e06bb1e87045aef
Gabriel Rojas <gabo06123@gmail.com>
2022-07-07 12:03:01 -0400

    init project

---
+++ services/frontend/src/main.js
@@ -0,0 +1,11 @@
+import "bootstrap/dist/css/bootstrap.css";
+import axios from "axios";
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = "http://localhost:5000/";
+
+createApp(App).use(store).use(router).mount("#app");
---
+++ services/frontend/src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "HomeView",
+  data() {
+    return {
+      msg: "",
+    };
+  },
+  created() {
+    this.getMessage();
+  },
+  methods: {
+    getMessage() {
+      axios
+        .get("/")
+        .then((res) => {
+          this.msg = res.data;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  },
+};
+</script>
